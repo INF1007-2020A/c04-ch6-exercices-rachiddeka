97d4ddc741dd6ffac285a2d7b25347b303b39f86
rachiddeka <chaidadeka@gmail.com>
2020-10-17 19:45:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def anagrams(words: list = None) -> bool:
     while len(words) < 2 :
         words.append(input("Entrez un mot\n"))
 
-    return False
+    return sorted()
 
 
 def contains_doubles(items: list) -> bool:
