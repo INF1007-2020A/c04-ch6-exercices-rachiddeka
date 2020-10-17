ef477b1f762a1fe5f97f73cb75a3c34b97c4381b
rachiddeka <chaidadeka@gmail.com>
2020-10-17 20:30:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,12 +25,8 @@ def anagrams(words: list = None) -> bool:
 
 
 def contains_doubles(items: list) -> bool:
-    res = False
-    for i in range(items) :
-        for j in items :
-            if i == j :
-                res = True
-    return res
+    
+    return 
 
 
 def best_grades(student_grades: dict) -> dict:
